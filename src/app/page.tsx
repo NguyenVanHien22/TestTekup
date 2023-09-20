1ebc9fbbd4ae38e4cf61e9517e7df5ed54cd5b20
Nguyễn Văn Hiển <nguyenvanhien22022@gmail.com>
2023-09-20 11:06:25 +0700

    add section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,29 @@
+"use client"
 import Image from 'next/image'
+import { useState } from 'react'
+import { ChevronRight } from 'lucide-react'
+import { menu, menuItems } from '@/constants/menu'
+
 
 export default function Home() {
+  const [currentSelection, setCurrentSelection] = useState(1);
   return (
-    <main className='flex min-h-screen flex-col items-center justify-between px-72 py-32 bg-white'>
-      <div className='w-full text-center'>
-        <a href="#" className='text-[18px] px-4 py-2 font-semibold rounded-lg bg-white'>All</a>
+    <main className='min-h-screen items-center justify-between max-w-[1300px] mx-auto py-32 bg-white px-8'>
+      <div className='w-full text-center mb-[60px]'>
+        {menu.map((item) => {
+            return <a href="#" className={`text-[18px] px-4 py-2 font-semibold inline-block rounded-lg ${currentSelection === item.id ? `bg-[#F8D000] shadow-md` : `bg-white`}`} onClick={() => setCurrentSelection(item.id)}>{item.name}</a>
+        })}
+      </div>
+      <div className='w-full grid lg:grid-cols-3 gap-[32px] md:grid-cols-2'>
+        {menuItems.filter((x) => x.category.includes(currentSelection)).map((item) => 
+          <div key={item.id} className='col-span-1 w-full h-[400px] bg-white rounded-lg shadow-xl overflow-hidden relative cursor-pointer hover:-translate-y-1 transition-all duration-300 hover:shadow-2xl'>
+            <img src={item.imageUrl} alt="" className='h-full w-full object-cover'/>
+            <div className='m-4 p-4 bg-white shadow-lg rounded-lg absolute bottom-0 w-[calc(100%-32px)] font-semibold flex justify-between items-center'>
+                <div>{item.title}</div>
+                <ChevronRight size={14}></ChevronRight>
+            </div>
+          </div> 
+            )}
       </div>
     </main>
   )
---
+++ src/constants/menu.ts
@@ -0,0 +1,87 @@
+export const menu = [
+    {
+      id: 1,
+      name: "All", 
+    },
+    {
+      id: 2,
+      name: "Manpower Supply", 
+    },
+    {
+      id: 3,
+      name: "Mobile App / Websites", 
+    },
+    {
+      id: 4,
+      name: "UI/UX Design", 
+    },
+  ]
+  
+  export const menuItems = [
+    {
+      id: 1,
+      imageUrl: "https://tekup.vn/wp-content/uploads/2022/09/image-1.png",
+      title: "TOI 3D Customize E-commerce",
+      category: [1,3],
+    },
+    {
+      id: 2,
+      imageUrl: "https://tekup.vn/wp-content/uploads/2022/09/image.png",
+      title: "E-learning – Internal training platform",
+      category: [1,3,4],
+    },
+    {
+      id: 3,
+      imageUrl: "https://tekup.vn/wp-content/uploads/2022/08/summer21-thumbnail.png",
+      title: "Summer 21 Cosmetic E-commerce Platform",
+      category: [1,3,4],
+    },
+    {
+      id: 4,
+      imageUrl: "https://tekup.vn/wp-content/uploads/2022/08/kiva-thumbnail.png",
+      title: "Kiva – Ambition to digital transformation in the brokerage assiduity",
+      category: [1,3,4],
+    },
+    {
+      id: 5,
+      imageUrl: "https://tekup.vn/wp-content/uploads/2022/08/boxgo-thumbnail.png",
+      title: "Boxgo – Professional Warehouse Management",
+      category: [1,3,4],
+    },
+    {
+      id: 6,
+      imageUrl: "https://tekup.vn/wp-content/uploads/2022/09/doitac-dsoft.jpg",
+      title: "Dsoft",
+      category: [2],
+    },
+    {
+      id: 7,
+      imageUrl: "https://tekup.vn/wp-content/uploads/2022/09/doitac-mitc.jpg",
+      title: "MITC",
+      category: [2],
+    },
+    {
+      id: 8,
+      imageUrl: "https://tekup.vn/wp-content/uploads/2022/09/doitac-tego.jpg",
+      title: "Tego",
+      category: [2],
+    },
+    {
+      id: 9,
+      imageUrl: "https://tekup.vn/wp-content/uploads/2022/09/doitac-mor.jpg",
+      title: "MOR",
+      category: [2],
+    },
+    {
+      id: 10,
+      imageUrl: "https://tekup.vn/wp-content/uploads/2022/09/doitac-code-complete.jpg",
+      title: "Code Complete",
+      category: [2],
+    },
+    {
+      id: 11,
+      imageUrl: "https://tekup.vn/wp-content/uploads/2022/09/doitac-smart-dev.jpg",
+      title: "Smart Dev",
+      category: [2],
+    },
+  ]
